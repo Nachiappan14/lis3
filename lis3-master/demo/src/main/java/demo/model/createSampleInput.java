15ded466c10b74e16a602076c3739d6b5ca2651c
Nachiappan14 <44224013+Nachiappan14@users.noreply.github.com>
2022-05-14 20:03:03 +0530

    added the required setters for the variables

--- lis3-master/demo/src/main/java/demo/model/createSampleInput.java
+++ lis3-master/demo/src/main/java/demo/model/createSampleInput.java
@@ -1,30 +1,35 @@
 package demo.model;
 
 public class createSampleInput {
-	 private String uid;
-	 private String sample_type;
-   private Integer quantity;
-   private String station_id;
+	private String uid;
+	private String sample_type;
+   	private Integer quantity;
+	private String station_id;
    
-  public String getUid() {
-		return uid;
+	public String getUid() {
+		return this.uid;
 	}
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
+	
 	public String getSampleType(){
 		return sample_type;
 	}
+	public void setSampleType(sample_type){
+		this.sample_type=sample_type;
+	}
+	
 	public Integer getQuantity() {
-		return quantity;
+		return this.quantity;
 	}
 	public void setQuantity(Integer number) {
 		this.quantity = number;
 	}
 
 	public String getStationId(){
-			return this.station_id;
-		}
+		return this.station_id;
+	}
 
 	public void setStationId(String sid){
 		this.station_id = sid;
