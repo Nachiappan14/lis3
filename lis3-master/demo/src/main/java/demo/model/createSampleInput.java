f6f2d9fbe42ee497601a3f0689e76af47ad75a58
Nachiappan14 <44224013+Nachiappan14@users.noreply.github.com>
2022-05-14 18:42:51 +0530

    Create createSampleInput.java

---
+++ lis3-master/demo/src/main/java/demo/model/createSampleInput.java
@@ -0,0 +1,21 @@
+package demo.model;
+
+public class createSampleInput {
+	 private String uid;
+	 private String sample_type;
+   private Integer quantity;
+   
+  public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+  
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer number) {
+		this.quantity = number;
+	}
+}
